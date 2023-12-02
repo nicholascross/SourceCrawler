08a77de05168daf599162ef4f2535a93620d9b2c
Nicholas Cross <nicholas@nacrossweb.com>
2023-12-02 22:06:17 +1100

    Decompose crawl source function

--- Sources/SourceCrawlerKit/SwiftSourceCrawler.swift
+++ Sources/SourceCrawlerKit/SwiftSourceCrawler.swift
@@ -16,39 +16,48 @@ public struct SwiftSourceCrawler {
         var results = [String: FileAnalysisResult]()
 
         if let enumerator = fileManager.enumerator(at: URL(fileURLWithPath: rootPath),
-                                                   includingPropertiesForKeys: [.isRegularFileKey],
-                                                   options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
+                                                    includingPropertiesForKeys: [.isRegularFileKey],
+                                                    options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
             for case let fileURL as URL in enumerator {
-                do {
-                    let fileAttributes = try fileURL.resourceValues(forKeys: [.isRegularFileKey])
-                    if fileAttributes.isRegularFile ?? false {
-                        let filePath = fileURL.path
-                        let fileExtension = fileURL.pathExtension
-
-                        if self.acceptedExtensions.contains(fileExtension) {
-                            if let fileContents = try? String(contentsOf: fileURL) {
-                                let analysisResult: FileAnalysisResult
-                                
-                                if fileExtension == "swift" {
-                                    let typeAnalysis = analyzer.analyze(fileContents: fileContents)
-                                    analysisResult = .swiftFile(types: typeAnalysis, content: fileContents)
-                                } else {
-                                    analysisResult = .otherFile(content: fileContents)
-                                }
-                               
-                                let relativePath = String(filePath.dropFirst(rootPath.count))
-                                results[relativePath] = analysisResult
-                            }
-                        }
-                    }
-                } catch {
-                    print("Failed to process file: \(fileURL.path), error: \(error)")
+                if let analysisResult = processFile(fileURL) {
+                    let relativePath = String(fileURL.path.dropFirst(rootPath.count))
+                    results[relativePath] = analysisResult
                 }
             }
         }
 
         return results
     }
+
+    private func processFile(_ fileURL: URL) -> FileAnalysisResult? {
+        do {
+            let fileAttributes = try fileURL.resourceValues(forKeys: [.isRegularFileKey])
+            if fileAttributes.isRegularFile ?? false {
+                let fileExtension = fileURL.pathExtension
+                if acceptedExtensions.contains(fileExtension) {
+                    return analyzeFileContent(at: fileURL, withExtension: fileExtension)
+                }
+            }
+        } catch {
+            print("Failed to process file: \(fileURL.path), error: \(error)")
+        }
+        return nil
+    }
+
+    private func analyzeFileContent(at fileURL: URL, withExtension fileExtension: String) -> FileAnalysisResult? {
+        if let fileContents = try? String(contentsOf: fileURL) {
+            if fileExtension == "swift" {
+                let typeAnalysis = analyzer.analyze(fileContents: fileContents)
+                return .swiftFile(types: typeAnalysis, content: fileContents)
+            } else {
+                return .otherFile(content: fileContents)
+            }
+        } else {
+            print("Failed to read contents of file: \(fileURL.path)")
+            return nil
+        }
+    }
+
 }
 
 
