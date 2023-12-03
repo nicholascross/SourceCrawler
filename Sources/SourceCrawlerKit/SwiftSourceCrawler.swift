85d9f4a3218db90adc8468106a1fafbc3bde4309
Nicholas Cross <nicholas@nacrossweb.com>
2023-12-03 13:19:14 +1100

    Breakdown file enumeration logic

--- Sources/SourceCrawlerKit/SwiftSourceCrawler.swift
+++ Sources/SourceCrawlerKit/SwiftSourceCrawler.swift
@@ -3,11 +3,11 @@ import FilenameMatcher
 
 public struct SwiftSourceCrawler {
     
-    let rootPath: String
-    let acceptedExtensions: [String]
-    let excludedPaths: [String]
-    let fileManager = FileManager.default
-    let analyzer = SwiftTypeAnalyser()
+    private let rootPath: String
+    private let acceptedExtensions: [String]
+    private let excludedPaths: [String]
+    private let fileManager = FileManager.default
+    private let analyzer = SwiftTypeAnalyser()
     
     public init(rootPath: String, acceptedExtensions: [String], excludedPaths: [String] = []) {
         self.rootPath = rootPath
@@ -18,9 +18,7 @@ public struct SwiftSourceCrawler {
     public func crawlSource() throws -> [String: FileAnalysisResult] {
         var results = [String: FileAnalysisResult]()
 
-        if let enumerator = fileManager.enumerator(at: URL(fileURLWithPath: rootPath),
-                                                    includingPropertiesForKeys: [.isRegularFileKey],
-                                                    options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
+        if let enumerator = fileEnumerator() {
             for case let fileURL as URL in enumerator {
                 guard acceptFileForProcessing(fileURL: fileURL) else { continue }
                 
@@ -35,11 +33,8 @@ public struct SwiftSourceCrawler {
     
     private func acceptFileForProcessing(fileURL: URL) -> Bool {
         let validExtension = acceptedExtensions.contains(fileURL.pathExtension)
-        lazy var excludedPath = excludedPaths.first(where: {
-            FilenameMatcher(pattern: $0).match(filename: fileURL.pathComponents.joined(separator: "/").replacingOccurrences(of: "//", with: "/"))
-        }) != nil
-        lazy var isRegularFile = (try? fileURL.resourceValues(forKeys: [.isRegularFileKey]).isRegularFile) ?? false
-        return validExtension && !excludedPath && isRegularFile
+        lazy var excludedPath = excludedPaths.isFilenameMatch(fileURL: fileURL)
+        return validExtension && !excludedPath && fileURL.isFileURL
     }
 
     private func analyzeFileContent(at fileURL: URL) throws -> FileAnalysisResult {
@@ -55,10 +50,33 @@ public struct SwiftSourceCrawler {
             throw SwiftSourceCrawlerError.failedToReadContents(fileURL)
         }
     }
-
+    
+    private func fileEnumerator() -> FileManager.DirectoryEnumerator? {
+        fileManager.enumerator(at: URL(fileURLWithPath: rootPath),
+                               includingPropertiesForKeys: [.isRegularFileKey],
+                               options: [.skipsHiddenFiles, .skipsPackageDescendants]
+        )
+    }
 }
 
 enum SwiftSourceCrawlerError: Error {
     case failedToReadContents(URL)
-    case unprocessibleFile(URL)
+}
+
+private extension URL {
+    var simplePath: String {
+        pathComponents.joined(separator: "/").replacingOccurrences(of: "//", with: "/")
+    }
+    
+    var isFile: Bool {
+        (try? resourceValues(forKeys: [.isRegularFileKey]).isRegularFile) ?? false
+    }
+}
+
+private extension Collection<String> {
+    func isFilenameMatch(fileURL: URL) -> Bool {
+        contains(where: {
+            FilenameMatcher(pattern: $0).match(filename: fileURL.simplePath)
+        })
+    }
 }
